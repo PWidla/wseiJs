d7e98257912ca7569ecf29fe18f6cf3f2ef93df0
PWidla <patrykwidlazb@gmail.com>
2023-10-17 19:33:59 +0200

    Wersja dasięludziompokazać:

--- lab2/magic.js
+++ lab2/magic.js
@@ -1,8 +1,25 @@
 const buttons = document.querySelectorAll('.slideNav');
+const prevBtn = document.querySelector('#prev')
+const nextBtn = document.querySelector('#next')
+const pauseBtn = document.querySelector('#pause')
+const playBtn = document.querySelector('#play')
 const sliderWrapper = document.querySelector('#slider-wrapper');
 let currentSlide = 1;
 let slideWidth = 600;
 let translateXValue = 0;
+let isPaused = false;
+let interval;
+
+function startSlider(){
+    interval = setInterval(() =>
+    {
+        if(!isPaused){
+            nextBtn.click();
+        }
+    }, 1500);
+}
+
+startSlider();
 
 function doTheMagic(slideValue) {
     const targetTranslateX = -slideWidth * (slideValue - 1);
@@ -13,13 +30,33 @@ function doTheMagic(slideValue) {
 }
 
 function handleButtonClick(event) {
-    if (event.target.id === "prev" && currentSlide > 1) {
+    if(event.target.id==="play"){
+        isPaused = false;
+    }
+    else if(event.target.id==="pause"){
+        isPaused = true;
+    }
+    else if (event.target.id === "prev" && currentSlide > 1) {
+        if(!isPaused){
+            clearInterval(interval);
+        }
         doTheMagic(currentSlide - 1);
+        if(!isPaused){
+            startSlider();
+        }
+
     } else if (event.target.id.startsWith("slide")) {
         const valueOfThisSlide = parseInt(event.target.id.replace("slide", ""), 10);
         doTheMagic(valueOfThisSlide);
+        
     } else if (event.target.id === "next" && currentSlide < 5) {
+        if(!isPaused){
+            clearInterval(interval);
+        }
         doTheMagic(currentSlide + 1);
+        if(!isPaused){
+            startSlider();
+        }
     } else if (event.target.id === "next" && currentSlide === 5) {
         doTheMagic(1);
     }
