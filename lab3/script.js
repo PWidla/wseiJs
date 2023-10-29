5ac34deef2140e61b74c02bf8d40f3ac15a10bf7
PWidla <patrykwidlazb@gmail.com>
2023-10-29 16:49:00 +0100

    Create script.js

---
+++ lab3/script.js
@@ -0,0 +1,87 @@
+const KeyToSound = {
+    'a': document.querySelector('#s1'),
+    's': document.querySelector('#s2'),
+    'd': document.querySelector('#s3'),
+    'f': document.querySelector('#s4'),
+}
+
+const recordingChannels = {
+    channel1: [],
+    channel2: [],
+    channel3: [],
+    channel4: [],
+}
+
+let activeChannel;
+
+function onKeyPress(event) {
+    const sound = KeyToSound[event.key];
+    if (sound) {
+        playSound(sound);
+        recordSound(sound, activeChannel);
+    }
+    console.log(sound);
+    const activeChannelContent = recordingChannels[activeChannel];
+    console.log(`Zawartość kanału ${activeChannel}:`, activeChannelContent);
+}
+
+function setActiveChannel(channel) {
+    activeChannel = channel;
+}
+
+function playSound(sound) {
+    sound.currentTime = 0;
+    sound.play();
+}
+
+function recordSound(sound, channel) {
+    let channelRetrieved = recordingChannels[channel];
+    if (channelRetrieved) {
+        const currentTime = Date.now();
+        if (channelRetrieved.length > 0) {
+            const delay = currentTime - (channelRetrieved[channelRetrieved.length - 1].timestamp);
+            recordingChannels[channel].push({ sound, delay, timestamp: currentTime });
+        } else {
+            recordingChannels[channel].push({ sound, delay: 0, timestamp: currentTime });
+        }
+    }
+}
+
+
+function startRecording(channel) {
+    recordingChannels[channel] = [];
+    setActiveChannel(channel);
+}
+
+function stopRecording(channel) {
+    recordingChannels[channel] = recordingChannels[channel] || [];
+}
+
+async function playChannel(channel) {
+    const playedChannel = recordingChannels[channel];
+
+    for (let i = 0; i < playedChannel.length; i++) {
+        const record = playedChannel[i];
+        await new Promise((resolve) => setTimeout(() => {
+            playSound(record.sound);
+            resolve();
+        }, record.delay));
+    }
+}
+
+function playAllChannels() {
+    for (let channel in recordingChannels) {
+        playChannel(channel);
+    }
+}
+
+function playSelectedChannels(){
+    let checkedRadios = document.querySelectorAll('input[type=radio]:checked');
+    for (let radio of checkedRadios) {
+        let id = radio.id;
+        let channel = id.slice(0, -5);
+        playChannel(channel);
+    }
+}
+
+document.addEventListener('keypress', onKeyPress);
